cd63167a86a232f0f284001b5c34a4c11939c379
Matthew Wiser <matt.wiser@icloud.com>
2022-07-11 02:15:32 -0500

    added latitude longitude conversion and API request based on selected city

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 // API key c58ad9defef708e43c489a22c4980616
 
-fetch('https://api.openweathermap.org/data/2.5/onecall?lat=38.7267&lon=-9.1403&exclude=current,hourly,minutely,alerts&units=imperial&appid=c58ad9defef708e43c489a22c4980616')
-.then(response => response.json())
-.then(data => console.log(data));
+// example current forecast + 7 day
+// fetch('https://api.openweathermap.org/data/2.5/onecall?lat=38.7267&lon=-9.1403&exclude=hourly,minutely,alerts&units=imperial&appid=c58ad9defef708e43c489a22c4980616')
+// .then(response => response.json())
+// .then(data => console.log(data));
+
+
+
+
+function formSubmitHandler(e){
+    e.preventDefault();
+    const selectedCity = $("#city").val();
+
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${selectedCity}&appid=c58ad9defef708e43c489a22c4980616`)
+    .then(response => response.json())
+    .then(function (data){
+        requestForecast(data[0]);
+    });
+}
+
+function requestForecast(latLon) {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latLon.lat}&lon=${latLon.lon}&exclude=hourly,minutely,alerts&units=imperial&appid=c58ad9defef708e43c489a22c4980616`)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+$("#cityForm").on("submit", formSubmitHandler);
 

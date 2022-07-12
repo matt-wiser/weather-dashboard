bec96a9d397bbb00547d21aa841dff3fc0282f1e
Matthew Wiser <matt.wiser@icloud.com>
2022-07-12 00:11:02 -0500

    final UI refinements

--- script.js
+++ script.js
@@ -42,12 +42,12 @@ function fetchGeoCode(city) {
             console.log("A new city!");
             cityHistory.splice(0, 0, city);
             localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
-            currentCityEl.textContent = city;
+            currentCityEl.textContent = `${city} (${moment().format("MM/DD/YYYY")})`;
             populateBtns();
             requestForecast(data[0]);
         
         } else if (data[0]) {
-            currentCityEl.textContent = city;
+            currentCityEl.textContent = `${city} (${moment().format("MM/DD/YYYY")})`;
             requestForecast(data[0]);
         }
         
@@ -60,16 +60,55 @@ function fetchGeoCode(city) {
 
 function populateUI(data) {
     console.log(data);
+    const forecastArray = data.daily;
+    const forecastContainer = document.getElementById("forecast-container");
+
     const currentTemp = document.getElementById("temp");
     const currentWind = document.getElementById("wind");
     const currentHumidity = document.getElementById("humidity");
     const currentUV = document.getElementById("uv");
-    
+    const weatherGlyph = document.getElementById("glyph");
 
     currentTemp.textContent = "Temperature: " + data.current.temp + " Degrees";
     currentWind.textContent = "Wind: " + data.current.wind_speed;
     currentHumidity.textContent = "Humidity: " + data.current.humidity + "%";
     currentUV.textContent = "UV Index: " + data.current.uvi;
+    weatherGlyph.setAttribute("src", `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`);
+    weatherGlyph.classList.remove("hidden");
+
+    const dailyWeather = document.getElementById("daily-info");
+    dailyWeather.classList.add("outline");
+
+    const fiveDay = document.getElementById("5day");
+    fiveDay.classList.remove("hidden");
+
+    removeAllChildNodes(forecastContainer);
+    for (let i = 1; i < 6; i++) {
+        const forecastDay = document.createElement("div");
+        forecastDay.classList.add("forecast");
+
+        const date = document.createElement("h3");
+        date.textContent = moment.unix(forecastArray[i].dt).format("MM/DD/YYYY");
+        forecastDay.append(date);
+
+        const glyph = document.createElement("img");
+        glyph.setAttribute("src", `http://openweathermap.org/img/wn/${forecastArray[i].weather[0].icon}@2x.png`);
+        forecastDay.append(glyph);
+
+        const temp = document.createElement("h3");
+        temp.textContent = `Temp: ${forecastArray[i].temp.day} Degrees`;
+        forecastDay.append(temp);
+
+        const wind = document.createElement("h3");
+        wind.textContent = `Wind: ${forecastArray[i].wind_speed} MPH`;
+        forecastDay.append(wind);
+
+        const humidity = document.createElement("h3");
+        humidity.textContent = `Humidity: ${forecastArray[i].humidity}%`;
+        forecastDay.append(humidity);
+
+        forecastContainer.append(forecastDay);
+    }
 }
 
 function populateBtns(){
